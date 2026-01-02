05d47e721b46acd024c2f109200c831d1a58ea2a
Daniel Sil <daniel.sil@kiwi.com>
2026-01-02 03:05:42 +0000

    fix: date calculation

--- frontend/src/components/TimelineEntry.tsx
+++ frontend/src/components/TimelineEntry.tsx
@@ -1,29 +1,6 @@
 import Image from "next/image";
 import { Connections } from "./Timeline";
 
-function formattedDateByTrackIndex(index: number) {
-  const date = new Date(Date.UTC(2026));
-  date.setDate(date.getDate() + Math.max(0, index - 3));
-  const day = date.getDate();
-  const month = date.toLocaleString("en-US", { month: "short" });
-
-  const getSuffix = (n: number) => {
-    if (n > 3 && n < 21) return "th";
-    switch (n % 10) {
-      case 1:
-        return "st";
-      case 2:
-        return "nd";
-      case 3:
-        return "rd";
-      default:
-        return "th";
-    }
-  };
-
-  return `${month} ${day}${getSuffix(day)}`;
-}
-
 export type TimelineEntryProps = {
   index: number;
   connections: Connections;
@@ -41,6 +18,30 @@ function TimelineEntry({
     return <div>nothing here</div>;
   }
 
+  const getFormattedDate = () => {
+    const date = new Date(Date.UTC(2026));
+    const positionFromEnd = tracks.length - 1 - index;
+    date.setDate(date.getDate() + Math.max(0, positionFromEnd - 2));
+    const day = date.getDate();
+    const month = date.toLocaleString("en-US", { month: "short" });
+
+    const getSuffix = (n: number) => {
+      if (n > 3 && n < 21) return "th";
+      switch (n % 10) {
+        case 1:
+          return "st";
+        case 2:
+          return "nd";
+        case 3:
+          return "rd";
+        default:
+          return "th";
+      }
+    };
+
+    return `${month} ${day}${getSuffix(day)}`;
+  };
+
   const track = tracks[index].track;
   const albumImages = track.album.images || [];
   const albumImage = albumImages[0]?.url;
@@ -57,7 +58,7 @@ function TimelineEntry({
       <div id="spotify-iframe"></div>
       <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
         <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-slate-900 rounded-full">
-          {formattedDateByTrackIndex(index)}
+          {getFormattedDate()}
         </time>
         <div className="text-xl font-bold text-white-900">{track.name}</div>
       </div>
