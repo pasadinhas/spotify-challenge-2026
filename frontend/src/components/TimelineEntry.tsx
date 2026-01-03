da582ecc4fb3cc0b34af25d94aaa82a11c9a0e73
Daniel Sil <daniel.sil@kiwi.com>
2026-01-03 01:47:22 +0000

    feat: display who published the song

--- frontend/src/components/TimelineEntry.tsx
+++ frontend/src/components/TimelineEntry.tsx
@@ -9,6 +9,12 @@ export type TimelineEntryProps = {
   setSelectedTrack: (trackId: string) => void;
 };
 
+const USERS = {
+  bukjir: "Carlos",
+  "1167349358": "Daniel",
+  "1173496040": "Miguel",
+};
+
 function TimelineEntry({
   index,
   tracks,
@@ -50,6 +56,7 @@ function TimelineEntry({
   };
 
   const track = tracks[index].track;
+  const addedBy = USERS[tracks[index].added_by.id as keyof typeof USERS];
   const albumImages = track.album.images || [];
   const albumImage = albumImages[0]?.url;
   const trackUri = track.uri;
@@ -115,6 +122,7 @@ function TimelineEntry({
   };
 
   console.log({
+    addedBy,
     connections,
     track,
     uri: track.uri,
@@ -124,9 +132,14 @@ function TimelineEntry({
     <div className="relative pl-8 sm:pl-32 py-6 group">
       <div id="spotify-iframe"></div>
       <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-        <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-slate-900 rounded-full">
-          {getFormattedDate()}
-        </time>
+        <div className="flex flex-row sm:flex-col items-center sm:items-start gap-2 sm:gap-0 sm:absolute left-0 translate-y-0.5 text-xs font-semibold mb-3 sm:mb-0">
+          <time className="uppercase px-1 py-0.5 inline-flex text-emerald-600 bg-slate-900 rounded-full">
+            {getFormattedDate()}
+          </time>
+          <span className="text-sm text-slate-500 px-1 py-0.5 normal-case">
+            {addedBy}
+          </span>
+        </div>
         <div className="text-xl font-bold text-white">{track.name}</div>
       </div>
 
